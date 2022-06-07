2a1df1a495b18b052a529c0efb8f75e484c5b5f6
Yu-Jung Cheng <thisisyujungcheng@gmail.com>
2022-06-07 12:56:21 +1000

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,14 +11,17 @@ run_zookeeper="$run --network kafka-net -h zookeeper --name zookeeper \
     -e ALLOW_ANONYMOUS_LOGIN=yes \
     bitnami/zookeeper:latest"
 
-run_kafka="$run docker run -d --network kafka-net -h kafka --name kafka \
-    -e ALLOW_PLAINTEXT_LISTENER=yes \
-    -e KAFKA_BROKER_ID=1 \
-    -e KAFKA_CFG_ZOOKEEPER_CONNECT=zookeeper:2181 \
-    -e KAFKA_CFG_LISTENERS=PLAINTEXT://:9092 \
-    -e KAFKA_CFG_ADVERTISED_LISTENERS=PLAINTEXT://127.0.0.1:9092 \
-    -p 9092:9092 \
-    bitnami/kafka:latest"
+run_kafka="$run --network kafka-net -h kafka --name kafka -p 9092:9092 \
+-e KAFKA_CFG_AUTO_CREATE_TOPICS_ENABLE=true \
+-e KAFKA_BROKER_ID=1 \
+-e KAFKA_LISTENERS=PLAINTEXT://:9092 \
+-e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://127.0.0.1:9092 \
+-e ALLOW_PLAINTEXT_LISTENER=yes \
+-e KAFKA_ZOOKEEPER_CONNECT=zookeeper:2181 \
+bitnami/kafka:latest"
 
 sh -c "${run_zookeeper}"
 sh -c "${run_kafka}"
+
+
+
