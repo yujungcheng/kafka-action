ff67a9e11d2455cb572b718454b8bf33ffed1c50
Yu-Jung Cheng <thisisyujungcheng@gmail.com>
2022-06-07 10:59:31 +1000

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
 
-echo "Set topic: ${INPUT_TOPIC_NAME}"
-echo ${INPUT_TOPIC_NAME} > /input_topic_name
-
 docker network create kafka-net --driver bridge
 
 docker run -d --network kafka-net -h zookeeper --name zookeeper \
@@ -18,18 +15,3 @@ docker run -d --network kafka-net -h kafka --name kafka \
     -e KAFKA_CFG_ADVERTISED_LISTENERS=PLAINTEXT://127.0.0.1:9092 \
     -p 9092:9092 \
     bitnami/kafka:latest
-
-sleep 10  # wait for broker to start.
-
-cat /input_topic_name
-INPUT_TOPIC_NAME=$(cat /input_topic_name)
-
-docker exec kafka kafka-topics.sh \
-    --create \
-    --topic ${INPUT_TOPIC_NAME} \
-    --replication-factor 1 \
-    --partitions 1 \
-    --bootstrap-server 127.0.0.1:9092
-
-topic_desc=$(docker exec kafka kafka-topics.sh --describe --topic ${INPUT_TOPIC_NAME} --bootstrap-server 127.0.0.1:9092)
-echo "::set-output name=topic_desc::${topic_desc}"
