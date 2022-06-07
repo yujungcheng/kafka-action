7cc946612166fe4e5a749c8bdc6360d5858dcd5b
Yu-Jung Cheng <thisisyujungcheng@gmail.com>
2022-06-07 12:44:17 +1000

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,31 @@
 #!/bin/sh
 
+
 docker network create kafka-net --driver bridge
 
-docker run -d --network kafka-net -h zookeeper --name zookeeper \
-    -e ALLOW_ANONYMOUS_LOGIN=yes \
-    bitnami/zookeeper:latest
+sh -c "docker network create kafka-net --driver bridge"
 
-docker run -d --network kafka-net -h kafka --name kafka \
+run="docker run -d"
+
+run_zookeeper="$run --network kafka-net -h zookeeper --name zookeeper \
+    -e ALLOW_ANONYMOUS_LOGIN=yes \
+    bitnami/zookeeper:latest"
+
+run_kafka="$run docker run -d --network kafka-net -h kafka --name kafka \
     -e ALLOW_PLAINTEXT_LISTENER=yes \
     -e KAFKA_BROKER_ID=1 \
     -e KAFKA_CFG_ZOOKEEPER_CONNECT=zookeeper:2181 \
     -e KAFKA_CFG_LISTENERS=PLAINTEXT://:9092 \
     -e KAFKA_CFG_ADVERTISED_LISTENERS=PLAINTEXT://127.0.0.1:9092 \
     -p 9092:9092 \
-    bitnami/kafka:latest
+    bitnami/kafka:latest"
 
-docker exec kafka kafka-topics.sh \
+sh -c "${run_zookeeper}"
+sh -c "${run_kafka}"
+
+sh -c "docker exec kafka kafka-topics.sh \
     --create \
     --topic monitor \
     --replication-factor 1 \
     --partitions 1 \
-    --bootstrap-server 127.0.0.1:9092
+    --bootstrap-server 127.0.0.1:9092"
