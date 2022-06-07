b42b05e0ff86beeb354219ae0991014974c5fed8
Yu-Jung Cheng <thisisyujungcheng@gmail.com>
2022-06-07 13:29:59 +1000

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,3 +7,14 @@ sh -c "$run_zookeeper"
 
 run_kafka="$docker_run -d -p $INPUT_KAFKA_PORT:9092 -e KAFKA_BROKER_ID=1 -e KAFKA_LISTENERS=PLAINTEXT://:9092 -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://127.0.0.1:9092  -e ALLOW_PLAINTEXT_LISTENER=yes -e KAFKA_ZOOKEEPER_CONNECT=zookeeper:2181 bitnami/kafka:$INPUT_KAFKA_VERSION"
 sh -c "$run_kafka"
+
+
+
+echo "TOPIC NAME: $INPUT_TOPIC_NAME"
+
+docker exec kafka kafka-topics.sh \
+    --create \
+    --topic monitor \
+    --replication-factor 1 \
+    --partitions 1 \
+    --bootstrap-server 127.0.0.1:9092
