38ee8c0d0606c0bf0bf182208c74c15d225d5569
Yu-Jung Cheng <thisisyujungcheng@gmail.com>
2022-06-07 14:09:20 +1000

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,29 @@
 #!/bin/sh
 
+docker network create kafka-net --driver bridge
+
 docker_run="docker run -d"
 
-run_zookeeper="$docker_run --name zookeeper -p $INPUT_ZOOKEEPER_PORT:2181 -e ALLOW_ANONYMOUS_LOGIN=yes bitnami/zookeeper:$INPUT_ZOOKEEPER_VERSION"
+run_zookeeper="$docker_run --name zookeeper -p $INPUT_ZOOKEEPER_PORT:2181 --network kafka-net \
+    -e ALLOW_ANONYMOUS_LOGIN=yes \
+    bitnami/zookeeper:$INPUT_ZOOKEEPER_VERSION"
 sh -c "$run_zookeeper"
 
-run_kafka="$docker_run --name kafka -p $INPUT_KAFKA_PORT:9092 -e KAFKA_BROKER_ID=1 -e KAFKA_LISTENERS=PLAINTEXT://:9092 -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://127.0.0.1:9092  -e ALLOW_PLAINTEXT_LISTENER=yes -e KAFKA_ZOOKEEPER_CONNECT=zookeeper:2181 bitnami/kafka:$INPUT_KAFKA_VERSION"
+run_kafka="$docker_run --name kafka -p $INPUT_KAFKA_PORT:9092 --network kafka-net \
+    -e KAFKA_BROKER_ID=1 \
+    -e KAFKA_LISTENERS=PLAINTEXT://:9092 \
+    -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://127.0.0.1:9092  \
+    -e ALLOW_PLAINTEXT_LISTENER=yes \
+    -e KAFKA_ZOOKEEPER_CONNECT=zookeeper:2181 \
+    bitnami/kafka:$INPUT_KAFKA_VERSION"
 sh -c "$run_kafka"
 
-echo "TOPIC NAME: $INPUT_TOPIC_NAME"
 
+echo "TOPIC NAME: $INPUT_TOPIC_NAME"
+docker ps
+echo "`date` sleep 10 seconds"
 sleep 10
+echo "`date` create topic"
 
 docker exec kafka kafka-topics.sh \
     --create \
